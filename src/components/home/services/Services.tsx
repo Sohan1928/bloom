35a7d27993241f44357c46e56e63c6c649204740
Sohan1928 <sohanurrahman1928@gmail.com>
2025-09-17 01:18:32 +0600

    services section responsive complete

--- src/components/home/services/Services.tsx
+++ src/components/home/services/Services.tsx
@@ -6,27 +6,27 @@ const Services = () => {
         <h2 className="title">What We Grow Best</h2>
       </div>
       {/* branding content */}
-      <div className="min-h-screen flex items-center justify-center">
-        <div className="branding-content grid grid-cols-4 gap-2 items-center justify-center text-center mx-auto">
+      <div className="lg:min-h-screen flex items-center justify-center">
+        <div className="branding-content grid lg:grid-cols-4 gap-2 items-center justify-center text-center mx-auto">
           <div className="branding-box">
             <h4>Branding</h4>
           </div>
-          <div className="branding-box available-box hidden lg:block">
+          <div className="branding-box available-box">
             <h4></h4>
           </div>
           <div className="branding-box">
             <h4>UI.UX</h4>
           </div>
-          <div className="branding-box available-box hidden lg:block">
+          <div className="branding-box available-box">
             <h4></h4>
           </div>
-          <div className="branding-box available-box hidden lg:block">
+          <div className="branding-box available-box">
             <h4></h4>
           </div>
           <div className="branding-box">
             <h4>Motion</h4>
           </div>
-          <div className="branding-box available-box hidden lg:block">
+          <div className="branding-box available-box">
             <h4></h4>
           </div>
           <div className="branding-box">
@@ -35,13 +35,13 @@ const Services = () => {
           <div className="branding-box">
             <h4>Mobile</h4>
           </div>
-          <div className="branding-box available-box hidden lg:block">
+          <div className="branding-box available-box">
             <h4></h4>
           </div>
           <div className="branding-box">
             <h4>3D</h4>
           </div>
-          <div className="branding-box available-box hidden lg:block">
+          <div className="branding-box available-box">
             <h4></h4>
           </div>
         </div>
