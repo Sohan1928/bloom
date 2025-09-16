71869755ae8a77c693d40e3a6f6a1358c6c4ccc7
Sohan1928 <sohanurrahman1928@gmail.com>
2025-09-16 19:31:26 +0600

    faq section complete and updated

--- src/components/home/faq/Faq.tsx
+++ src/components/home/faq/Faq.tsx
@@ -4,7 +4,9 @@ const Faq = () => {
   return (
     <section className="faq-container">
       <div className="faq-content">
-        <h2>Faq</h2>
+        <div>
+          <h2>Faq</h2>
+        </div>
         <div>
           <Accordion></Accordion>
         </div>
